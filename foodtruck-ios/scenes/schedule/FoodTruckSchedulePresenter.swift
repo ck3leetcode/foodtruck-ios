20df1bec4986f5f281512b59e6a46afe65b198a2
Chris K <chriskwok@local>
2019-11-28 10:05:03 -0800

    init presenter and view controller for food truck map scene.

--- foodtruck-ios/scenes/schedule/FoodTruckSchedulePresenter.swift
+++ foodtruck-ios/scenes/schedule/FoodTruckSchedulePresenter.swift
@@ -6,8 +6,32 @@
 //  Copyright © 2019 Chris Kwok. All rights reserved.
 //
 
-import UIKit
+import Foundation
+
+protocol FoodTruckSchedulePresentingView {
+    func update(schedules: [FoodTruckSchedule])
+    func updateError(errMsg: String)
+}
 
 class FoodTruckSchedulePresenter {
-
+    private let dataProvider: FoodTruckScheduleDataProvider
+    private let presentingView: FoodTruckSchedulePresentingView
+    
+    init(presentingView: FoodTruckSchedulePresentingView,
+         dataProvider: FoodTruckScheduleDataProvider = SocrataFoodTruckScheduleDataProvider()) {
+        self.presentingView = presentingView
+        self.dataProvider = dataProvider
+    }
+    
+    
+    func load() {
+        let now = Date()
+        dataProvider.fetch(by: now) { [weak self] (result) in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .failure(let error): strongSelf.presentingView.updateError(errMsg: "fail to fetch data:  \(error.localizedDescription)")
+            case .success(let schedules): strongSelf.presentingView.update(schedules: schedules)
+            }
+        }
+    }
 }
