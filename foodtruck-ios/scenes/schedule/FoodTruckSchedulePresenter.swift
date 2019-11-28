ea96f488e30c91660b9854baef6b95b8960c2655
Chris K <chriskwok@local>
2019-11-28 10:03:13 -0800

    init presenter and view controller for food truck schedule scene.

---
+++ foodtruck-ios/scenes/schedule/FoodTruckSchedulePresenter.swift
@@ -0,0 +1,13 @@
+//
+//  FoodTruckSchedulePresenter.swift
+//  foodtruck-ios
+//
+//  Created by Chris Kwok on 11/28/19.
+//  Copyright © 2019 Chris Kwok. All rights reserved.
+//
+
+import UIKit
+
+class FoodTruckSchedulePresenter {
+
+}
