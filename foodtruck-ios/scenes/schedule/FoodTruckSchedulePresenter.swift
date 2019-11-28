e7dbd0798b835c9f059934959a5842bbb0436e7e
Chris K <chriskwok@local>
2019-11-28 13:01:06 -0800

    foodtruck-ios/scenes/schedule/FoodTruckSchedulePresenter.swift

--- foodtruck-ios/scenes/schedule/FoodTruckSchedulePresenter.swift
+++ foodtruck-ios/scenes/schedule/FoodTruckSchedulePresenter.swift
@@ -7,15 +7,25 @@
 //
 
 import Foundation
+import MapKit
 
-protocol FoodTruckSchedulePresentingView {
-    func update(schedules: [FoodTruckSchedule])
+protocol FoodTruckSchedulePresentingView: class {
+    func update(viewModels: [FoodTruckScheduleViewModel])
     func updateError(errMsg: String)
+}
+
+struct FoodTruckScheduleViewModel {
+    let name: String
+    let address: String
+    let menu: String
+    let hours: String
+    let location: CLLocation
+    let selected: Bool
 }
 
 class FoodTruckSchedulePresenter {
     private let dataProvider: FoodTruckScheduleDataProvider
-    private let presentingView: FoodTruckSchedulePresentingView
+    private weak var presentingView: FoodTruckSchedulePresentingView?
     
     init(presentingView: FoodTruckSchedulePresentingView,
          dataProvider: FoodTruckScheduleDataProvider = SocrataFoodTruckScheduleDataProvider()) {
@@ -28,9 +38,22 @@ class FoodTruckSchedulePresenter {
         let now = Date()
         dataProvider.fetch(by: now) { [weak self] (result) in
             guard let strongSelf = self else { return }
-            switch result {
-            case .failure(let error): strongSelf.presentingView.updateError(errMsg: "fail to fetch data:  \(error.localizedDescription)")
-            case .success(let schedules): strongSelf.presentingView.update(schedules: schedules)
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    strongSelf.presentingView?.updateError(errMsg: "fail to fetch data: \(error.localizedDescription)")
+                case .success(let schedules):
+                    let vms = schedules.map { s in
+                        FoodTruckScheduleViewModel(
+                            name: s.name,
+                            address: s.address,
+                            menu: s.description,
+                            hours: "\(s.starttime) - \(s.endtime)",
+                            location: s.location,
+                            selected: false)
+                    }
+                    strongSelf.presentingView?.update(viewModels: vms)
+                }
             }
         }
     }
